e3404dfa7b36a92017f160224f144b3c5d1ec157
Alessandro <alessandro.corneli11@mail.com>
2020-01-07 16:24:19 +0100

    2

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,11 +1,18 @@
 var mail = prompt("Inserisci la tua mail:");
 console.log(mail);
 var archivioMail = ["1@example.com","2@example.com","3@example.com"];
+console.log(archivioMail[3]);
+var trovato = false;
 for (var i = 0; i < archivioMail.length; i++) {
-    if (archivioMail.includes(mail)) {
-      document.writeln("corretto");
-    }
-    else {
-      documenteriteln("errore");
-    }
+  var mailConsiderata = archivioMail[i]
+  if (mail==mailConsiderata) {
+    trovato = true;
+  }
+}
+console.log(trovato);
+if (trovato==false) {
+  document.writeln("errore");
+}
+else {
+  document.writeln("corretto");
 }
